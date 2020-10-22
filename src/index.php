24506e2645acd7722d11ab8a96ef9adb80c51087
Lewis Cowles <lewiscowles@me.com>
2020-10-22 16:38:28 +0100

    Version Bump

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Enable SVG Uploads
  * Plugin URI:        https://github.com/Lewiscowles1986/WordPressSVGPlugin
  * Description:       Enable SVG uploads in Media Library and other file upload fields.
- * Version:           2.0.2
+ * Version:           2.0.3
  * Author:            Lewis Cowles
  * Author URI:        https://www.lewiscowles.co.uk/
  * License:           GPL-3.0
