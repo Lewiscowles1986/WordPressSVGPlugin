bb16023f3fdc58170ebd202ce190fe4494878ea6
Lewis Cowles <lewis+github@cowles.uk>
2021-05-29 05:24:25 +0100

    Modify test suite bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,7 @@ php composer.phar install --dev
 ");
 }
 
-require_once __DIR__ . '/../vendor/autoload.php';
+define('ABSPATH', __DIR__ . '/../src');
+
 require_once __DIR__ . '/../src/index.php';
 require_once __DIR__ . '/includes/BaseWPMockTestCase.php';
