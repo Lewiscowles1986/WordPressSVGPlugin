6ba21b8101d2dc3652916409525afaf97d0f325b
Lewis Cowles <lewis+github@cowles.uk>
2021-05-29 05:39:51 +0100

    Adjust test bootstrap
    Why was composer being included twice...

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,4 @@
 <?php
 
-if (!@include(__DIR__ . '/../vendor/autoload.php')) {
-    die("You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install --dev
-");
-}
-
 require_once __DIR__ . '/../src/index.php';
 require_once __DIR__ . '/includes/BaseWPMockTestCase.php';
